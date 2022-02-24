98e51bfbfaaf41c473c7decd85ffe5f700f068d3
Icaro de Barros <icarodebarros@gmail.com>
2022-02-24 14:03:38 -0300

    Environment file

---
+++ src/environments/environment.js
@@ -0,0 +1,6 @@
+export const ENV = {
+  /* Using Firebase */
+  URL_REALTIME_DB: "https://<PATH_HERE>.firebaseio.com",
+  API_KEY: "<KEY_HERE>",
+  URL_SIGNUP: "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=",
+}
