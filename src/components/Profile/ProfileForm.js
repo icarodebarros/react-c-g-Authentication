b62c88de16de2c46ed8b22b05ccd90051f9a70af
Icaro de Barros <icarodebarros@gmail.com>
2022-02-24 15:02:46 -0300

    Update password

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,48 @@
+import { useRef, useContext } from 'react';
+import { ENV } from '../../environments/environment';
+
+import AuthContext from '../../store/auth-context';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+  const newPasswordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    // add validation
+
+    fetch(ENV.URL_UPDATE_PASSWORD + ENV.API_KEY, {
+      method: 'POST',
+      body: JSON.stringify({
+        idToken: authCtx.token,
+        password: enteredNewPassword,
+        returnSecureToken: false
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => {
+      // assumption: Always succeeds!
+
+
+    });
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' minLength="7" ref={newPasswordInputRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
--- src/environments/environment.js
+++ src/environments/environment.js
@@ -4,4 +4,5 @@ export const ENV = {
   API_KEY: "<KEY_HERE>",
   URL_SIGNUP: "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=",
   URL_LOGIN: "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=",
+  URL_UPDATE_PASSWORD: "https://identitytoolkit.googleapis.com/v1/accounts:update?key=",
 }
